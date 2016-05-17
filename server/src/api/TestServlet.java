f98db1386067aaad1f9348b2d735eaff5b62f9c1
Michael Pearson <zl2mjp@gmail.com>
2016-05-18 09:44:54 +1200

    Added bootstrap

--- server/src/api/TestServlet.java
+++ server/src/api/TestServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class TestServlet extends javax.servlet.http.HttpServlet {
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.getWriter().println("Hello world");
+        response.getWriter().println(request.getParameter("Test"));
+
     }
+
 }
