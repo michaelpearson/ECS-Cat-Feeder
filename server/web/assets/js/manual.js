5d8c188fe86a02c13458e4834b161f3654e2f901
Michael <zl2mjp@gmail.com>
2016-08-08 09:29:23 +1200

    Changed js. Still need to react to feeder change & tidy

--- server/web/assets/js/manual.js
+++ server/web/assets/js/manual.js
@@ -1,34 +1,26 @@
 var pages = window.pages || {};
 pages.manual = {
-    foodTypeCount : 2,
     renderPage : function (pageArguments, renderCompleteCallback) {
         var me = pages.manual;
         me.renderCompleteCallback = renderCompleteCallback;
         $('#manual-control-page.page').css({
             display : 'block'
         });
-        var foodDeliveryHtml = "<div class=\"row food-type\">\n    <div class=\"col-md-4\">\n        <h3 style=\"margin: 0;\">Food Type <span class=\"index\"></span></h3>\n    </div>\n    <div class=\"col-md-3\">\n        <div class=\"input-group\" style=\"width: 80%\">\n            <input class=\"form-control\" type=\"number\" step=\"1.00\" placeholder=\"0.00\"/>\n            <span class=\"input-group-addon\">grams</span>\n        </div>\n    </div>\n    <div class=\"col-md-5\">\n        <button class=\"btn btn-default\">Deliver</button>\n    </div>\n</div>"
+        var foodDeliveryHtml = "<div class=\"row food-type\">\n    <div class=\"col-md-4\">\n        <h3 style=\"margin: 0;\"><span class=\"food-type-name\"></span></h3>\n    </div>\n    <div class=\"col-md-3\">\n        <div class=\"input-group\" style=\"width: 80%\">\n            <input class=\"form-control\" type=\"number\" step=\"1.00\" placeholder=\"0.00\"/>\n            <span class=\"input-group-addon\">grams</span>\n        </div>\n    </div>\n    <div class=\"col-md-5\">\n        <button class=\"btn btn-default\">Deliver</button>\n    </div>\n</div>"
 
-        getAllFeeders(function (response) {
-            if(!response.success) {
-                return;
-            }
-            var el = $('.food-delivery-panel');
-            el.children().remove();
+        var el = $('.food-delivery-panel');
+        el.children().remove();
 
-            var feeders = response.catFeeders || [];
-            for(var b = 0;b < feeders.length;b++) {
-                for(var a = 0;a < me.foodTypeCount;a++) {
-                    var foodTypeEl = $(foodDeliveryHtml);
-                    el.append(foodTypeEl);
+        var feeder = app.getFeederInfo();
+        for(var a = 0;a < feeder.foodTypes.length;a++) {
+            var foodTypeEl = $(foodDeliveryHtml);
+            el.append(foodTypeEl);
 
-                    foodTypeEl.find('.index').text(a);
-                    foodTypeEl.find('.btn').click(function (index, hardware_id) {
-                        me.deliverFood(this.find('input[type=number]').val(), index, hardware_id);
-                    }.bind(foodTypeEl, a, feeders[b].hardwareId));
-                }
-            }
-        });
+            foodTypeEl.find('.food-type-name').text(feeder.foodTypes[a].name);
+            foodTypeEl.find('.btn').click(function (index, hardware_id) {
+                me.deliverFood(this.find('input[type=number]').val(), index, hardware_id);
+            }.bind(foodTypeEl, feeder.foodTypes[a].id, feeder.hardwareId));
+        }
     },
     deliverFood : function (amount, type, hardwareId) {
         deliverFood(amount, type, hardwareId, function () {
