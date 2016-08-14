ffa2ccf60809c7e53f051663f82806231f893839
Robbie <robbiefraser@hotmail.com>
2016-08-14 16:43:29 +1200

    Added modifying food types

--- server/web/assets/js/settings.js
+++ server/web/assets/js/settings.js
@@ -6,11 +6,8 @@ pages.settings = {
         $('#settings-page.page').css({
             display : 'block'
         });
-        var foodTypes = $('settings-edit-food-type');
-        foodTypes.children().remove();
-        $(app.getFeederInfo().foodTypes).each(function (i, element) {
-            foodTypes.append('<option value="' + element.id + '">' + element.name + '</option>');
-        });
+        me.type1 = app.getFeederInfo().foodTypes[0];
+        me.type2 = app.getFeederInfo().foodTypes[1];
         me.initControls();
     },
     initControls : function () {
@@ -20,37 +17,39 @@ pages.settings = {
         }
         me.init = true;
 
-        me.foodTypeAl = $('#settings-add-food-type');
-        me.amountAl = $('#settings-add-deliver-amount');
-        me.foodTypeEl = $('#settings-edit-food-type');
-        me.amountEl = $('#settings-edit-deliver-amount');
+        var name1 = $('#settings-food-one-name');
+        var name2 = $('#settings-food-two-name');
+        var default1 = $('#settings-food-one-default');
+        var default2 = $('#settings-food-two-default');
+        var indicator1 = $('#settings-food-one-indicator');
+        var indicator2 = $('#settings-food-two-indicator');
 
-        var addAmountIndicator = $('#settings-add-amount-indicator');
-        me.amountAl.on('input', function () {
-            addAmountIndicator.text(me.amountAl.val() + " grams");
+        var button1 = $('#settings-food-one-update');
+        var button2 = $('#settings-food-two-update');
+
+        name1.val(me.type1.name);
+        name2.val(me.type2.name);
+        default1.val(me.type1.defaultGramAmount);
+        default2.val(me.type2.defaultGramAmount);
+
+        button1.click(function(){
+            me.updateFood(name1.val(), default1.val(), 1)
+        });
+
+        button2.click(function(){
+            me.updateFood(name2.val(), default2.val(), 2)
+        });
+
+        default1.on('input', function(){
+            indicator1.text(default1.val() + 'grams')
+        }).trigger('input');
+        default2.on('input', function(){
+            indicator2.text(default2.val() + 'grams')
         }).trigger('input');
 
-        var editAmountIndicator = $('#settings-edit-amount-indicator');
-        me.amountEl.on('input', function () {
-            editAmountIndicator.text(me.amountEl.val() + " grams");
-        }).trigger('input');
 
-        $('#settings-add-food').click(me.addFood);
-        $('#settings-edit-food').click(me.updateFood);
     },
-    addFood : function () {
-        var me = pages.settings;
-        var id = app.getCurrentFeederId();
-        var amount = me.amountAl.val();
-        var type = me.foodTypeAl.val();
-        console.log(amount, type, id);
-        //post new food type
-    },
-    updateFood : function () {
-        var me = pages.settings;
-        var defaultAmount = me.amountEl.val();
-        var foodTypeId = me.foodTypeEl.val();
-        foodTypeId = 1;
-        updateFoodType(foodTypeId, defaultAmount, "New Name " + Math.random(), app.invalidateFeederInfo);
+    updateFood : function (name, def, typeId) {
+        updateFoodType(typeId, def, name, app.invalidateFeederInfo);
     }
 };
