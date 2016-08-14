69ecdfb34faac79295e92199f744477cee3bc3d6
Michael <zl2mjp@gmail.com>
2016-08-14 23:20:35 +1200

    Make the maual page default to the default amount of food

--- server/web/assets/js/manual.js
+++ server/web/assets/js/manual.js
@@ -7,20 +7,21 @@ pages.manual = {
     tagListEl : null,
     tagItemTemplate : '<div class="form-group">\n    <label class="col-sm-2 control-label">Tag</label>\n    <div class="input-group">\n        <span class="input-group-addon tag-uid"></span>\n        <input type="text" class="form-control tag-name" aria-label="New tag name" title="New tag name">\n        <div class="input-group-btn">\n            <button class="btn btn-warning auth">Set as authorised</button>\n            <button class="btn btn-primary save">Save</button>\n        </div>\n    </div>\n</div>',
     foundTags : [],
+    renderCompleteCallback : null,
     renderPage : function (pageArguments, renderCompleteCallback) {
         var me = pages.manual;
-        renderCompleteCallback();
+        me.initControls();
+        me.renderCompleteCallback = renderCompleteCallback;
         $('#manual-control-page.page').css({
             display : 'block'
         });
-        var foodTypeEl = $('#manual-food-type');
-        foodTypeEl.children().remove();
+        me.foodTypeEl.children().remove();
         $(app.getFeederInfo().foodTypes).each(function (i, element) {
-            foodTypeEl.append('<option value="' + element.id + '">' + element.name + '</option>')
+            me.foodTypeEl.append('<option value="' + element.id + '" data-default="' + element.defaultGramAmount + '">' + element.name + '</option>')
         });
+        me.foodTypeEl.change();
         me.timerId = setInterval(me.detectTags, 4000);
         me.detectTags();
-        me.initControls();
     },
     unload : function () {
         clearInterval(pages.manual.timerId);
@@ -29,7 +30,6 @@ pages.manual = {
         var me = pages.manual;
         getLastCardId(app.getCurrentFeederId(), function (response) {
             var tag = response.tag || {};
-            var isPresent = response.isPresent || false;
             if(!tag || !tag.tagUID || tag.tagUID == 0) {
                 return;
             }
@@ -46,6 +46,9 @@ pages.manual = {
             el.find('button.auth').click(me.setAuthorised.bind(this, tag, nameEl));
             me.tagListEl.append(el);
             me.foundTags.push(tag);
+            if(me.renderCompleteCallback == null) {
+                me.renderCompleteCallback();
+            }
         });
     },
     initControls : function () {
@@ -64,6 +67,12 @@ pages.manual = {
         me.amountEl.on('input', function () {
             amountIndicator.text(me.amountEl.val() + " grams");
         }).trigger('input');
+
+        me.foodTypeEl.change(function () {
+            var defaultAmount = ($(this).find('option:selected').attr('data-default'));
+            me.amountEl.val(defaultAmount);
+            me.amountEl.trigger('input');
+        });
     },
     updateCard : function (tag, nameEl, callback) {
         tag.tagName = nameEl.val();
